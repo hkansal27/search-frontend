394470f27bf4f162af00f53479aa6004f0a89b1c
Himanshu  Kansal <himanshu@trihund.com>
2019-05-29 15:34:42 +0530

    added search child components

--- src/app/modules/search/search.module.ts
+++ src/app/modules/search/search.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchContainerComponent } from './components/search-container/search-container.component';
 import { SearchRoutingModule } from './search-routing.module';
+import { SearchInputComponent } from './components/search-input/search-input.component';
+import { SearchResultComponent } from './components/search-result/search-result.component';
 
 @NgModule({
   imports: [
     CommonModule,
     SearchRoutingModule
   ],
-  declarations: [SearchContainerComponent]
+  declarations: [SearchContainerComponent, SearchInputComponent, SearchResultComponent]
 })
 export class SearchModule { }
